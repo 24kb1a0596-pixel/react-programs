974b74d62e4ab427dfd17f89d7e6a7d8d58c6ec2
24kb1a0596 <24kb1a0596@nbkrist.org>
2026-09-24 09:38:06 +0530

    Add React programs

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import Simpleform from './Simpleform'
+function App() 
+{
+  return (
+  <div>
+     <Simpleform/>
+  </div>
+  )
+}
+export default App
